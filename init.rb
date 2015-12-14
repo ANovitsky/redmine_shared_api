c8e40aae29e704d3dc2526c7362629aac4a21fa6
Tony <anovitsky@gmail.com>
2015-12-14 09:14:01 +0200

    updated version

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :redmine_shared_api do
   name 'Redmine Shared API'
   author 'Ahau Software'
-  description 'This is a plugin share API for Redmine Outlook Add-Шn'
+  description 'This is a plugin share API for Redmine Outlook Add-In, QuickBooks, etc..' 
   version '0.0.2'
   url 'https://github.com/anovitsky/redmine_shared_api'
   author_url 'http://www.ahausoftware.com'
