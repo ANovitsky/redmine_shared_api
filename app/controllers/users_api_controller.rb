de0c275863cd3fab1a213a8ca17763d425854acc
ANovitsky <ANovitsky@gmail.com>
2018-08-15 12:06:38 +0000

    extend UsersApiController, filter by status, like by name, filter by  group_id, render custom fields for users

--- app/controllers/users_api_controller.rb
+++ app/controllers/users_api_controller.rb
@@ -3,11 +3,26 @@ class UsersApiController < ApplicationController
   before_filter :require_login
   accept_api_auth :index
 
-  def index    
+  helper :sort
+  include SortHelper
+  helper :custom_fields
+  include CustomFieldsHelper
+  helper :principal_memberships
+  
+  def index   
+	sort_init 'firstname', 'asc'
+    sort_update %w(firstname lastname created_on)
+	
+	@status = params[:status] || 1
+
+    scope = User.logged.status(@status).preload(:email_address)
+    scope = scope.like(params[:name]) if params[:name].present?
+    scope = scope.in_group(params[:group_id]) if params[:group_id].present?
+	
+	@users =  scope.order(sort_clause).limit(@limit).offset(@offset).to_a
+	
     respond_to do |format|      
-      format.api {
-        @users = User.all
-      }
+      format.api 
     end      
   end
   
