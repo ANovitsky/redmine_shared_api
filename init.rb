a620a1405cebf673f3ee4e22d520084664c81715
Antony Novitsky <anton@ahausoftware.com>
2019-03-20 14:14:55 -0100

    changed version

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_shared_api do
   name 'Redmine Shared API'
   author 'Ahau Software'
   description 'This is a plugin share API for Redmine Outlook Add-In, QuickBooks, etc..' 
-  version '0.0.9'
+  version '0.1.1'
   url 'https://github.com/anovitsky/redmine_shared_api'
   author_url 'https://www.ahausoftware.com'
   requires_redmine :version_or_higher => '2.4.0'
